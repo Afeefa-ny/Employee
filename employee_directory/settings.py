f824c666c896cf41c5c1172a8ea1bb27c4089a45
Afeefa ny <afeefany2004@gmail.com>
2026-03-17 21:22:09 +0530

    added Procfile

--- employee_directory/settings.py
+++ employee_directory/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -49,6 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+ 
 ]
 
 ROOT_URLCONF = 'employee_directory.urls'
@@ -70,7 +74,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'employee_directory.wsgi.application'
 
+ALLOWED_HOSTS = ['*']  # later you can restrict
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
